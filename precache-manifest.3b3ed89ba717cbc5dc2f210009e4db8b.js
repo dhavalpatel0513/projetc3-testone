114c806c100df187f5bfb4ab908cbe4c2cc37a5a
Dhaval Patel <dhavalpatel@Dhavals-MacBook-Pro.local>
2020-01-25 12:22:04 -0500

    Updates

---
+++ precache-manifest.3b3ed89ba717cbc5dc2f210009e4db8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c142b0c33f80bd750508abc7fdec85ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ea2fc0d7908b30ac12",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e3b2df6bf43c8abe2fd3",
+    "url": "/static/js/2.7f38fdf4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7f38fdf4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14ea2fc0d7908b30ac12",
+    "url": "/static/js/main.3486af77.chunk.js"
+  },
+  {
+    "revision": "533b22c224f7e1a4d740",
+    "url": "/static/js/runtime-main.e72a04bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
